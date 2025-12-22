e1c501ccff277c607638467e2e9f2697fd822cc9
Lem57Y <yury.i.yashchenko@gmail.com>
2025-12-22 19:01:50 +0300

    почти всё движение написано

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from telebot.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from config import API_TOKEN
 
 keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=False)
-button = KeyboardButton(text = "Left")
-button1 = KeyboardButton(text = "Right")
-button2 = KeyboardButton(text = "Restart")
+button = KeyboardButton(text = "на север")
+button1 = KeyboardButton(text = "на юг")
+button2 = KeyboardButton(text = "на восток")
+button3 = KeyboardButton(text = "на запад")
+button4 = KeyboardButton(text = "счётчик гейгера")
 keyboard.add(button)
 keyboard.add(button1)
 keyboard.add(button2)
-room = 0
-
+keyboard.add(button3)
+storage = {}
 
 
 
@@ -20,126 +22,171 @@ bot = telebot.TeleBot(API_TOKEN)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 
+
+
+
 def send_welcome(message):
-    global room
+    global storage
+    iD = message.from_user.id
     first_text = ("Hello") + (' ') + message.from_user.first_name + (' ') + ('@') + message.from_user.username
+    #keyboard = ReplyKeyboardMarkup()
+    #keyboard.add(button)
     bot.send_message(
         message.chat.id,
         first_text,
-        reply_markup=keyboard)
-    room = 1
+        reply_markup=keyboard
+    )
+    storage[iD] = {"room": 1, 'key': 1, 'code': 0, 'rooms': {1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 0: 0}}
+    
+    
     
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message: Message):
-    global room
+    global storage
     text = message.text
-    if text == "Restart": 
-        bot.send_message(
-            message.from_user.id,
-            'The game has been restarted',
-        )
-        room = 1
-    else:
-        if room == 1:
-            if text == "Left":
-                bot.send_message(
-                    message.from_user.id,
-                    'You went left from room' + str(room),
-                )
-                room = 2
+    iD = message.from_user.id
+    room = storage[iD]["room"]
 
-            elif text == "Right":
-                bot.send_message(
-                    message.from_user.id,
-                    'You went right from room' + str(room),
-                )
-                room = 3
-                
-            else:
-                bot.send_message(
-                    message.from_user.id,
-                    "You presed someting wrong",
-                )
-
-    
-        elif room == 2:
-            if text == "Left":
-                bot.send_message(
-                    message.from_user.id,
-                    'You went left from room' + str(room),
-                )
-                room = 4
-
-            elif text == "Right":
-                bot.send_message(
-                    message.from_user.id,
-                    'You went right from room' + str(room),
-                )
-                room = 1
-                
-            else:
-                bot.send_message(
-                    message.from_user.id,
-                    "You presed someting wrong",
-                )
-
-
-        elif room == 3:
-            if text == "Left":
-                bot.send_message(
-                    message.from_user.id,
-                    'You went left from room' + str(room),
-                )
-                room = 5
-
-            elif text == "Right":
-                bot.send_message(
-                    message.from_user.id,
-                    'You went right from room' + str(room),
-                )
-                room = 1
-                
-            else:
-                bot.send_message(
-                    message.from_user.id,
-                    "You presed someting wrong",
-                )
-
-
-        elif room == 4:
-            if text == "Left":
-                bot.send_message(
-                    message.from_user.id,
-                    'You went left from room' + str(room),
-                )
-                room = 3
-
-            elif text == "Right":
-                bot.send_message(
-                    message.from_user.id,
-                    'You went right from room' + str(room),
-                )
-                room = 2
-                
-            else:
-                bot.send_message(
-                    message.from_user.id,
-                    "You presed someting wrong",
-                )        
-
-
-        if room == 5:
+    if room == 1:
+        if text == 'на север':
             bot.send_message(
-                    message.from_user.id,
-                    "Congratulations, you escaped.",
-                )  
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 4
+            
+
+    elif room == 2:
+        if text == "на север":
             bot.send_message(
+                message.from_user.id,
+                'Вы прошли через пролом в стене ' + text,
+            )
+            storage[iD]["room"] = 8
+
+        elif text == "на запад":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 3
+
+    elif room == 4:
+        if text == "на север":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 6
+
+        elif text == "на юг":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 1
+
+        elif text == "на восток":
+            if storage[iD]['key'] == 1:
+                bot.send_message(
+                message.from_user.id,
+                'вы отрыли дверь ключ-картой и прошли ' + text,
+                )
+                storage[iD]["room"] = 5
+
+            else:
+                bot.send_message(
                     message.from_user.id,
-                    "if you want to play again, press restart",
-                )  
+                    'необходима ключ-карта'
+                )
+
+
+    elif room == 5:
+        if text == "на запад":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 4
+
+    elif room == 6:
+        if text == "на север":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 7
+        
+        elif text == "на запад":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 8
+
+        elif text == "на юг":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 4
+
+    elif room == 7:
+        if text == "на юг":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 6
+
+    elif room == 8:
+        if text == "на восток":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 6
+
+        elif text == "на запад":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 9
+
+        elif text == "на юг":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через пролом в стене ' + text,
+            )
+            storage[iD]["room"] = 2
+
+    elif room == 9:
+        if text == "на восток":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 8
+
+        if text == "на юг":
+            bot.send_message(
+                message.from_user.id,
+                'вы прошли через дверь ' + text,
+            )
+            storage[iD]["room"] = 3
+
+    print(storage[iD]["room"])
+
+
+        
+        
+
+
+
     
 
 bot.infinity_polling()
